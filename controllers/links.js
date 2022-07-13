f9e6e83d987be8477e63f333a088eed036cb18a2
Mikhail L <horrorgr+git@gmail.com>
2022-07-13 22:38:34 +0200

    Remove video

--- controllers/links.js
+++ controllers/links.js
@@ -6,10 +6,7 @@ const linksController = (bot, user) => async (msg) => {
   const lang = getLanguage(user);
   const loadingMsg = await bot.sendMessage(chatId, '...');
 
-  await bot.sendVideo(chatId, '../tg_02_en.mp4', {
-    width: 720,
-    height: 1280,
-    caption: lang.linksContent,
+  await bot.sendMessage(chatId, lang.linksContent, {
     reply_markup: {
       keyboard: inviteKeyboard(
         chatId, lang.invite, lang.nft, lang.myNft, lang.settings,
