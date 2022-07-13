0ef0eda89811c405d514b71da4c34f920b356e11
Mikhail L <horrorgr+git@gmail.com>
2022-07-13 20:16:17 +0200

    Add congrats

--- tasks/verifyPurchases.js
+++ tasks/verifyPurchases.js
@@ -12,35 +12,27 @@ const verifyPurchases = async (bot, repeat = true) => {
     
     uDataReq.rows.forEach(async (user) => {
       const pdb = new Pool();
-      console.log({
-        user
-      })
       const purchasesReq = await pdb.query(
         `select distinct tgid, initedByTgId, createdat, payed from purchases 
           where initedByTgId='${user.tgid}' 
           and payed='0' 
           and createdAt<='${Number(user.expiresat)}'
-          and createdAt>'${Number(user.expiresat) - (userExpiration * 24 * 60 * 60 * 1000)}'`
+          and createdAt>'${Number(user.expiresat) - (userExpiration * 24 * 60 * 60 * 1000)}' limit 3`
       );
       await pdb.end();
       const totalPurchases = purchasesReq.rowCount;
-      console.log(totalPurchases)
 
       if(totalPurchases >= amountToBuyForReward) {
-        console.log(2);
         const sendBack = await sendReturn(user.tgid);
 
         if (sendBack) {
-          console.log(3);
           const pdb = new Pool();
           purchasesReq.rows.forEach(async purchase => {
             await pdb.query(`update purchases set payed='1' where createdAt='${purchase.createdat}' and initedByTgId='${user.tgid}'`);
           });
           await pdb.end();
         }
-        console.log(4);
         await bot.sendMessage(user.tgid, 'You have a return ' + price + ' TON');
-        console.log(5);
       }
     });
 
