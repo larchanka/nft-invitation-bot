92c3e64eca9273450b83d6eebefcc6eba432be9a
Mikhail L <horrorgr+git@gmail.com>
2022-07-13 16:31:54 +0200

    Add logs

--- utils/createInvitation.js
+++ utils/createInvitation.js
@@ -3,16 +3,35 @@ const pdb = require("../connections/postgres");
 
 const createInvitation = async (fromId, toId) => {
   try {
+    console.log('select * from users where tgid=' + toId + ' and banned!=1')
     const pdb = new Pool();
     const userRes = await pdb.query('select * from users where tgid=' + toId + ' and banned!=1');
     const userExists = userRes?.rows[0];
-
+console.log('select * from invitations where toTgId=' + toId)
     const inviteRes = await pdb.query('select * from invitations where toTgId=' + toId);
     const invitedUserExists = inviteRes?.rows[0];
 
     if (!!userExists || !!invitedUserExists) {
       return null;
     }
+console.log(`INSERT INTO invitations (
+  fromTgId, 
+  toTgId, 
+  createdAt, 
+  expiresAt, 
+  activatedAt
+) VALUES (
+  ${fromId},
+  ${toId},
+  ${new Date().getTime()},
+  ${new Date().getTime() + 7 * 24 * 60 * 60 * 1000},
+  NULL,
+)`)
+
+
+console.log(`
+UPDATE users SET invitations=invitations-1
+WHERE tgid=${fromId};`)
 
     await pdb.query(`INSERT INTO invitations (
       fromTgId, 
