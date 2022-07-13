883610f64a3aa5fb8a10aa2a7c1d60346cb04dd7
Mikhail L <horrorgr+git@gmail.com>
2022-07-13 22:33:04 +0200

    Video test

--- controllers/links.js
+++ controllers/links.js
@@ -4,8 +4,12 @@ const inviteKeyboard = require('../utils/inviteKeyboard');
 const linksController = (bot, user) => async (msg) => {
   const chatId = msg.chat.id;
   const lang = getLanguage(user);
+  const loadingMsg = await bot.sendMessage(chatId, '...');
 
-  bot.sendMessage(chatId, lang.linksContent, {
+  await bot.sendVideo(chatId, '../tg_02_en.mp4', {
+    width: 720,
+    height: 1280,
+    caption: lang.linksContent,
     reply_markup: {
       keyboard: inviteKeyboard(
         chatId, lang.invite, lang.nft, lang.myNft, lang.settings,
@@ -15,6 +19,8 @@ const linksController = (bot, user) => async (msg) => {
       input_field_placeholder: lang.selectAction,
     },
   });
+
+  bot.deleteMessage(chatId, loadingMsg.message_id);
 };
 
 module.exports = linksController;
