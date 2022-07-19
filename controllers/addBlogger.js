9018b3c6b176ff63802ae11a1e2c2d29e2650834
Mikhail L <horrorgr+git@gmail.com>
2022-07-19 15:44:45 +0200

    Add blogger

--- controllers/addBlogger.js
+++ controllers/addBlogger.js
@@ -6,12 +6,17 @@ const addBloggerController = (bot, _user) => async (msg, match) => {
   const [, invitations, _, user] = match;
 
   try {
-    const userId = await getUserByLink(user);
-    const pdb = new Pool();
-    await pdb.query(`update users set level='2', invitations='${invitations}' where tgid=${userId}`);
-    await pdb.end();
 
-    bot.sendMessage(chatId, `Блоггер @${user} добавлен со ${invitations} приглашениями`)
+    if (chatId === process.env.OWNER) {
+      const userId = await getUserByLink(user);
+      const pdb = new Pool();
+      await pdb.query(`update users set level='2', invitations='${invitations}' where tgid=${userId}`);
+      await pdb.end();
+
+      bot.sendMessage(chatId, `Блоггер @${user} добавлен со ${invitations} приглашениями`);
+    } else {
+      bot.sendMessage(chatId, `Нет прав`);
+    }
   } catch (e) {
     console.log('addBloggerController.js Error', e.toString());
   }
